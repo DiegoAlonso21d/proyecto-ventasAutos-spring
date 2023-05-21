306586674650236154f4a99e855cf381622f5ec3
Diego Alonso <diegoalonso_21d@hotmail.com>
2023-05-20 21:16:13 -0500

    Primer commit

---
+++ src/main/java/com/proyect/ventasAuto/SistemaVentasAutoApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.ventasAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasAutoApplication.class, args);
+	}
+
+}
